c5b4c3076eac9bb94c2987e9b5de031e7a91260f
Yoshiteru Iwasaki <yoshiteru.i@gmail.com>
2014-12-24 15:12:30 +0900

    Update mysql.sh
    mkdir追加

--- MacOSX/Homebrew/bin/mysql.sh
+++ MacOSX/Homebrew/bin/mysql.sh
@@ -5,6 +5,7 @@ brew install mysql
 #設定変更
 sudo cp resources/my.cnf /usr/local/etc/my.cnf
 #自動起動設定
+mkdir -p ~/Library/LaunchAgents
 ln -sfv /usr/local/opt/mysql/*.plist ~/Library/LaunchAgents
 launchctl load ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist
 #パスワード設定
