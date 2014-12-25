335422223ca79e53466d6c9d66bb331effd690d0
Yoshiteru Iwasaki <yoshiteru.i@gmail.com>
2014-12-25 17:56:49 +0900

    Update eclipse.sh

--- MacOSX/Homebrew/bin/eclipse.sh
+++ MacOSX/Homebrew/bin/eclipse.sh
@@ -14,10 +14,10 @@ sudo cp -r ~/Downloads/pleiades/eclipse/features/* /opt/homebrew-cask/Caskroom/e
 sudo echo -javaagent:../../../dropins/MergeDoc/eclipse/plugins/jp.sourceforge.mergedoc.pleiades/pleiades.jar >> /Applications/Eclipse.app/Contents/MacOS/eclipse.ini
 
 #EclipseへTomcatプラグインインストール
-#wget -P ~/Downloads/ http://www.eclipsetotale.com/tomcatPlugin/tomcatPluginV331.zip
-#cd ~/Downloads/
-#unzip tomcatPluginV331.zip
-#sudo cp ~/Downloads/com.sysdeo.eclipse.tomcat_3.3.1.jar /opt/homebrew-cask/Caskroom/eclipse-ide/4.4.0/eclipse/dropins/
+wget -P ~/Downloads/ http://www.eclipsetotale.com/tomcatPlugin/tomcatPluginV331.zip
+cd ~/Downloads/
+unzip tomcatPluginV331.zip
+sudo cp ~/Downloads/com.sysdeo.eclipse.tomcat_3.3.1.jar /opt/homebrew-cask/Caskroom/eclipse-ide/4.4.1/eclipse/dropins/
 
 #EclipseへVelocityWebEditプラグインインストール
 wget  -P ~/Downloads/ "http://downloads.sourceforge.net/project/velocitywebedit/velocitywebedit/1.0.8/VelocityWebEdit-1.0.8.zip"
