8edda21e82debcd4645789658c211c3408691bfa
Yoshiteru Iwasaki <yoshiteru.i@gmail.com>
2014-12-26 13:42:57 +0900

    Update aipo.sh
    psqlコマンド追加

--- MacOSX/Homebrew/bin/aipo.sh
+++ MacOSX/Homebrew/bin/aipo.sh
@@ -4,3 +4,10 @@ echo setup aipo
 mkdir -p  ~/Documents/aipo/workspace
 mkdir -p  ~/Documents/aipo/GitHub
 mvn eclipse:add-maven-repo -Declipse.workspace=~/Documents/aipo/workspace
+
+echo clone aipo
+cd ~/Documents/aipo/GitHub
+git clone https://github.com/aipocom/aipo.git
+
+echo setup aipo database
+psql -Upostgres -d org001 < ~/Documents/aipo/GitHub/aipo/sql/postgres/org001.sql
