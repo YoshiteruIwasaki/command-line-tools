cc981ad399400ea3749fd8ff3887d96bfa2feb2a
Yoshiteru Iwasaki <yoshiteru.i@gmail.com>
2015-01-28 12:08:40 +0900

    Update java.sh
    Java8対応

--- MacOSX/Homebrew/bin/java.sh
+++ MacOSX/Homebrew/bin/java.sh
@@ -7,17 +7,23 @@ brew install maven2
 echo install ant
 brew install ant
 
-touch ~/.bash_profile
-
-echo JAVA_HOME指定
-echo export JAVA_HOME=`/usr/libexec/java_home -v "1.6"`  >> ~/.bash_profile
-echo PATH=${JAVA_HOME}/bin:${PATH}  >> ~/.bash_profile
-
-source ~/.bash_profile
-
 #Install Java7 for Eclipse
 echo Java7 for Eclipse
 wget -P ~/Downloads/ --no-check-certificate --no-cookies - --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7u71-b14/jdk-7u71-macosx-x64.dmg
 hdiutil mount ~/Downloads/jdk-7u71-macosx-x64.dmg
 sudo installer -pkg /Volumes/JDK\ 7\ Update\ 71/JDK\ 7\ Update\ 71.pkg -target /
 hdiutil unmount /Volumes/JDK\ 7\ Update\ 71/
+
+#Install Java8 for Eclipse
+wget -P ~/Downloads/ --no-check-certificate --no-cookies - --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u31-b13/jdk-8u31-macosx-x64.dmg
+hdiutil mount ~/Downloads/jdk-8u31-macosx-x64.dmg
+sudo installer -pkg /Volumes/JDK\ 8\ Update\ 31/JDK\ 8\ Update\ 31.pkg -target /
+hdiutil unmount /Volumes/JDK\ 8\ Update\ 31/
+
+touch ~/.bash_profile
+
+echo JAVA_HOME指定
+echo export JAVA_HOME=`/usr/libexec/java_home -v "1.8"`  >> ~/.bash_profile
+echo PATH=${JAVA_HOME}/bin:${PATH}  >> ~/.bash_profile
+
+source ~/.bash_profile
