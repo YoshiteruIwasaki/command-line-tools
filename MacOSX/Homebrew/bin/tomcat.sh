87cf286deab5b61e3796be68e16f40e1ebc093b9
Yoshiteru Iwasaki <yoshiteru.i@gmail.com>
2015-01-28 12:10:56 +0900

    Update tomcat.sh
    comet対応コメント追加

--- MacOSX/Homebrew/bin/tomcat.sh
+++ MacOSX/Homebrew/bin/tomcat.sh
@@ -19,3 +19,8 @@ cd /usr/local/opt/tomcat7/libexec/lib/
 echo install jdbc driver
 wget http://central.maven.org/maven2/org/postgresql/postgresql/9.3-1102-jdbc3/postgresql-9.3-1102-jdbc3.jar
 wget http://central.maven.org/maven2/mysql/mysql-connector-java/5.1.34/mysql-connector-java-5.1.34.jar
+
+echo Cometを有効にします
+echo protocol を org.apache.coyote.http11.Http11NioProtocol に変更します。
+echo protocol="org.apache.coyote.http11.Http11NioProtocol" 
+echo vim /usr/local/opt/tomcat7/libexec/conf/server.xml
