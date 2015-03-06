24ba7aa61c791b47157b04d8709fec467c2fddbd
Yoshiteru Iwasaki <yoshiteru.i@gmail.com>
2015-03-06 12:10:13 +0900

    Update test-portlet.sh
    add createdb

--- MacOSX/Homebrew/bin/test-portlet.sh
+++ MacOSX/Homebrew/bin/test-portlet.sh
@@ -9,3 +9,14 @@ cd ~/Documents/aipo/GitHub
 git clone https://github.com/aipocom/test-portlet.git
 cd~/Documents/aipo/GitHub/test-portlet
 mvn install
+
+
+#データベース作成
+echo データベースを作成します
+#PostgreSQL9.3の場合
+create database aipo_test owner postgres encoding 'UTF8';
+#PostgreSQL9.4の場合
+createdb -E UTF8 -O postgres -U postgres aipo_test;
+
+psql -Upostgres -d aipo_test < ~/Documents/aipo/GitHub/test-portlet/sql/postgres/org001.sql
+psql -Upostgres -d aipo_test < ~/Documents/aipo/GitHub/test-portlet/sql/postgres/eip_t_test.sql
