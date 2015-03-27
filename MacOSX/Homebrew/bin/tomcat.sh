e528a56d0102ccbc0af074835f9b766c3da1f381
Yoshiteru Iwasaki <yoshiteru.i@gmail.com>
2015-03-27 16:28:48 +0900

    Update tomcat.sh
    jdbc41に変更
    tomcat6削除

--- MacOSX/Homebrew/bin/tomcat.sh
+++ MacOSX/Homebrew/bin/tomcat.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
 
-echo install tomcat6
-brew install tomcat6
-brew link tomcat6 --force
-cd /usr/local/opt/tomcat6/libexec/lib/
-
-#jdbcインストール
-echo install jdbc driver
-wget http://central.maven.org/maven2/org/postgresql/postgresql/9.3-1102-jdbc3/postgresql-9.3-1102-jdbc3.jar
-wget http://central.maven.org/maven2/mysql/mysql-connector-java/5.1.34/mysql-connector-java-5.1.34.jar
-
 echo install tomcat7
 brew install tomcat7
 brew link tomcat7 --force
@@ -17,7 +7,7 @@ cd /usr/local/opt/tomcat7/libexec/lib/
 
 #jdbcインストール
 echo install jdbc driver
-wget http://central.maven.org/maven2/org/postgresql/postgresql/9.3-1102-jdbc3/postgresql-9.3-1102-jdbc3.jar
+wget http://central.maven.org/maven2/org/postgresql/postgresql/9.3-1101-jdbc41/postgresql-9.3-1101-jdbc41.jar
 wget http://central.maven.org/maven2/mysql/mysql-connector-java/5.1.34/mysql-connector-java-5.1.34.jar
 
 echo Cometを有効にします
